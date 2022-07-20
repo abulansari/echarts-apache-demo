2bdca1fefd6c65c82b7f596577d05f254ad994bc
abulansari <thecalion110@gmailcom>
2022-07-20 15:13:21 +0530

    SFDASFS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ReactECharts from 'echarts-for-react';
+import "./App.css"
 function App() {
+  // const options = {
+  //   grid: { top: 8, right: 8, bottom: 24, left: 36 },
+  //   xAxis: {
+  //     type: 'category',
+  //     data: ['jan', 'feb', 'march', 'april', 'may', 'jun', 'july  '],
+  //   },
+  //   yAxis: {
+  //     type: 'value',
+  //   },
+  //   series: [
+  //     {
+  //       data: [100, 400, 500, 4, 505, 802, 500],
+  //       type: 'line',
+  //       smooth: true,
+  //     },
+  //   ],
+  //   tooltip: {
+  //     trigger: 'axis',
+  //   },
+  // };
+
+  const options = {
+    xAxis: {
+      type: 'category',
+      data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
+    },
+    yAxis: {},
+    series: [
+      {
+        type: 'bar',
+        name: '2015',
+        data: [89.3, 92.1, 94.4, 85.4]
+      },
+      {
+        type: 'bar',
+        name: '2016',
+        data: [95.8, 89.4, 91.2, 76.9]
+      },
+      {
+        type: 'bar',
+        name: '2017',
+        data: [97.7, 83.1, 92.5, 78.1]
+      }
+    ]
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='echarts'>
+        <ReactECharts option={options} />
+      </div>
     </div>
   );
 }
